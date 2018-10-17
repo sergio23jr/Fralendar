d2eaaa0adb9caf0c6218280037eda6beed8ba078
Smushed <smushedcode@gmail.com>
2018-10-16 21:32:13 -0500

    Added Style and JS with links in the html

---
+++ assets/fralendar.js
@@ -0,0 +1,11 @@
+// Initialize Firebase
+var config = {
+    apiKey: "AIzaSyDO5T58hrlOuSJhKNvgOjeXnrvqWgT41MQ",
+    authDomain: "fralendar.firebaseapp.com",
+    databaseURL: "https://fralendar.firebaseio.com",
+    projectId: "fralendar",
+    storageBucket: "fralendar.appspot.com",
+    messagingSenderId: "391251924818"
+};
+firebase.initializeApp(config);
+
