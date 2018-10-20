38889cda7678ed170e384ab3998397c3eff0ec4d
Smushed <smushedcode@gmail.com>
2018-10-20 14:00:20 -0500

    Fixed a bug where new users couldn't select parts in their calendar

--- assets/fralendar.js
+++ assets/fralendar.js
@@ -1,24 +1,21 @@
 //Object which holds everyone's freetime
 var freetime = {};
-var userFreetime = {};
+var userFreeTime = {};
 
 // listener event for buttons 
-$(".btn").on("click", function () {
+$(".calendar-btn").on("click", function () {
     var value = $(this).val();
-    console.log(value)
     // changes value on button to "1" signifying they are available
     if (value === "0" || value === "2") {
         $(this).val("1")
         $(this).removeClass("btn-danger")
         $(this).addClass("btn-success");
-        console.log(value)
     }
     // changes value on button to "2" signifying they are unavailables
     if (value === "1") {
         $(this).val("2")
         $(this).removeClass("btn-success")
         $(this).addClass("btn-danger")
-        console.log(value)
     }
 
 });
@@ -26,10 +23,16 @@ $(".btn").on("click", function () {
 //Gets the User Calendar when authstatechanges from the login page
 function getUserCalendar() {
     firebase.database().ref(`/freetime/${user.ID}`).once("value").then(function (snap) {
+        userFreeTime = {};
         console.log(snap.val())
-        userFreetime = snap.val();
+        userFreeTime = snap.val();
     });
-}
+};
+
+function addNewUserToCalendar() {
+    userFreeTime = { "saturdayMorning": "0" }
+    firebase.database().ref(`/freetime/${user.ID}`).set(userFreeTime);
+};
 
 //Adds a listener to freetime so when new freetime is selected it pulls to the object
 firebase.database().ref("/freetime/").on("value", function (snap) {
@@ -40,7 +43,8 @@ $(".calendar-btn").on("click", function () {
     //Pulls the value and the ID from each button to be used in firebase
     var buttonTime = $(this).val();
     var attribute = $(this).attr(`id`);
-    userFreetime[attribute] = buttonTime;
-    freetime[user.ID] = userFreetime;
+    freetime[user.ID] = {};
+    userFreeTime[attribute] = buttonTime;
+    freetime[user.ID] = userFreeTime;
     firebase.database().ref(`/freetime/`).set(freetime);
 });
