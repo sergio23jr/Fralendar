08d295fb6fd166f27a96a5e619a8749741fb5993
Sergio Jr <sergio23jr@gmail.com>
2018-10-23 09:01:42 -0500

    adding events to document

--- assets/apis-events.js
+++ assets/apis-events.js
@@ -26,29 +26,119 @@ apiEvents = [];
 $.ajax({
   type: "GET",
   url: tmApiCall
-}).then(function(response) {
+}).then(function (response) {
   //log the queryURL
   console.log(response);
-  //log the result and specific paramters
-  console.log(tmApiCall);
-  //Pull in event name
-  console.log(response._embedded.events[0].name);
 
-  //Get Venue latitude for weather
-  console.log(
-    response._embedded.events[0]._embedded.venues[0].location.latitude
-  );
-  //Get venue longitutude for weather
-  console.log(
-    response._embedded.events[0]._embedded.venues[0].location.longitude
-  );
   apiEvents = response._embedded.events;
-  console.log(apiEvents);
-  //for loop to pull in all events for the week.
-  for (let z = 0; z < apiEvents.length; z++) {
-    console.log(apiEvents[z].name);
-    console.log(apiEvents[z].dates);
-    console.log(apiEvents[z].dates.start.localDate);
-    console.log(apiEvents[z].dates.start.localTime);
+
+  var eventPlace = response._embedded.events
+
+  //Loop populates document with events 
+  for (let i = 0; i < apiEvents.length; i++) {
+
+    // variable holds the time event takes place (24hr format HH:MM:SS)
+    var time = apiEvents[i].dates.start.localTime
+
+    //variable holds the date event takes place (YYYY-MM-DD)
+    var date = apiEvents[i].dates.start.localDate
+
+    // creates div to append all info of event along with needed attributes
+    var parentEvent = $("<div>");
+    parentEvent.attr({ "id": "event #" + (i + 1) })
+    parentEvent.addClass(["eventDiv", "row"])
+
+    //creates an img element along with needed attributes
+    var imgEvent = $("<img>")
+    imgEvent.attr({
+      "id": "eventImg",
+      "src": apiEvents[i].images[0].url,
+      "class": "col-md-4"
+    })
+
+    //creating a div for the remaining 8 columns needed per bootstrap
+    var divColumn = $("<div>")
+    divColumn.addClass(["col-md-8", "info" + ([i] + 1)])
+
+    //creaing a new row to insert Name of event with needed attributes
+    var divRowName = $("<div>")
+    divRowName.addClass("row")
+    divColumn.append(divRowName)
+
+    // create div to append event name to from array
+    var namespan = $("<div>");
+    namespan.addClass("col-md-12")
+    namespan.text("Event: " + apiEvents[i].name);
+
+    // append name to row 
+    $(divRowName).append(namespan);
+
+    //creating a new row to insert time and date with needed attributes
+    var divRowTime = $("<div>")
+    divRowTime.addClass("row")
+    divColumn.append(divRowTime)
+
+    // creating div for date and time 
+    var timespan = $("<div>");
+    timespan.addClass("col-md-12")
+
+    //set the text to date and time of event
+    timespan.text("When: " + getTimeAndDate(time, date));
+
+    //append to row
+    $(divRowTime).append(timespan);
+
+    // creating a new row to insert location with needed attributes
+    var divRowPlace = $("<div>")
+    divRowPlace.addClass("row")
+    divColumn.append(divRowPlace)
+
+    // create div that will hold location of event with need attributes
+    var placeDiv = $("<div>")
+    placeDiv.addClass("col-md-12")
+    placeDiv.text("Location: " + eventPlace[i]._embedded.venues[0].name + " " + eventPlace[i]._embedded.venues[0].address.line1)
+    $(divRowPlace).append(placeDiv)
+
+    //adding picture plus a new div to insert all other info
+    parentEvent.append([imgEvent, divColumn]);
+
+    //append event to document
+    $(".Events").append(parentEvent);
+  }
+
+  //Function inputs the date and time and reformats it 
+  function getTimeAndDate(eventTime, eventDate) {
+    // variable will hold the year the event takes place
+    var eventYear = eventDate.slice(0, 4)
+
+    // variable will hold the month the event takes place
+    var eventMonth = eventDate.slice(5, 7)
+
+    // variable will hold the day the event takes place
+    var eventDay = eventDate.slice(8, 10)
+
+    //New date with US format
+    eventDate = eventMonth + "/" + eventDay + "/" + eventYear
+
+    // this will get our hour the event takes place
+    var sliceNum = eventTime.slice(0, 2)
+
+    //variable to represent Am or Pm
+    var am_pm = "";
+
+    if (+eventTime.slice(0, 2) < 12) {
+      am_pm = "am"
+    } else {
+      // reassign variable to 12hr format for hour
+      sliceNum = (+eventTime.slice(0, 2) - 12)
+      am_pm = "pm"
+    }
+
+    // get the correct time format HH:MM
+    var correctTimeFormat = sliceNum + eventTime.slice(2, 5)
+
+    //Variable for correct date format MM/DD/YYYY at HH:MM
+    var correctDateFormat = eventDate + " at " + correctTimeFormat + am_pm
+    return correctDateFormat
   }
 });
